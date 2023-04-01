753fbb680abcb8865110d0369973c888450c7962
roman <belyakov1981@mail.ru>
2023-04-01 18:12:47 +0300

    Lesson 1

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,11 @@
 from blog.app import create_app
 
 app = create_app()
+
+
+# from blog.app import app
+# if __name__ == "__main__":
+#     app.run(
+#         host="0.0.0.0",
+#         debug=True,
+#     )
