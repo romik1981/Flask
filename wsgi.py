e2d72c40e9a44304783de9bb1da3762df91c46c6
roman <belyakov1981@mail.ru>
2023-04-05 01:06:30 +0300

    Lesson 2 first commit

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,26 @@ app = create_app()
 
 
 
+# from blog.app import create_app, db
+# from flask import redirect
+# from blog.models import User
+
+# app = create_app()
+
+
+# @app.route("/")
+# def index():
+#     return redirect('/users/')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='127.0.0.1',
+        debug=True,
+        port=5000
+    )
+
+
 
 
 # from blog.app import app
