5670463359468620b98f76a9cd83efe84cc41da4
roman <belyakov1981@mail.ru>
2023-04-06 00:22:23 +0300

    Lesson_2 Ready

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,73 +1,29 @@
-# from flask import Blueprint, render_template
-# from flask_login import login_required
-# from werkzeug.exceptions import NotFound
-
-# from blog.models import User
-
-# user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
-
-
-# @user.route('/')
-# def user_list():
-#     users = User.query.all()
-#     return render_template(
-#         'users/list.html',
-#         users=users,
-#     )
-
-
-# @user.route('/<int:pk>')
-# @login_required
-# def profile(pk: int):
-#     selected_user = User.query.filter_by(id=pk).one_or_none()
-#     if not selected_user:
-#         raise NotFound(f"User #{pk} doesn't exist!")
-
-#     return render_template(
-#         'users/profile.html',
-#         user=selected_user,
-#     )
-
-
-
-
-
-
-
 from flask import Blueprint, render_template
+from flask_login import login_required
 from werkzeug.exceptions import NotFound
 
-user = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
-USERS = {
-    1: {"name": "Ivan"},
-    2: {"name": "Jon"},
-    3: {"name": "Mary"}
-}
+from blog.models import User
+
+user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 
 
-@user.route("/")
+@user.route('/')
 def user_list():
+    users = User.query.all()
     return render_template(
-        "users/list.html",
-        users=USERS
+        'users/list.html',
+        users=users,
     )
 
 
-@user.route("/<int:pk>")
-def get_user(pk: int):
-    if pk in USERS:
-        user_raw = USERS[pk]
-    else:
-        raise NotFound("User id:{}, not found".format(pk))
+@user.route('/<int:pk>')
+@login_required
+def profile(pk: int):
+    selected_user = User.query.filter_by(id=pk).one_or_none()
+    if not selected_user:
+        raise NotFound(f"User #{pk} doesn't exist!")
+
     return render_template(
-        "users/details.html",
-        user_name=user_raw["name"]
+        'users/profile.html',
+        user=selected_user,
     )
-
-
-def get_user_name(pk: int):
-    if pk in USERS:
-        user_name = USERS[pk]["name"]
-    else:
-        raise NotFound("User id:{}, not found".format(pk))
-    return user_name
--- wsgi.py
+++ wsgi.py
@@ -2,22 +2,6 @@ from blog.app import create_app
 
 
 
-
-
-
-
-# from blog.app import create_app, db
-# from flask import redirect
-# from blog.models import User
-
-# app = create_app()
-
-
-# @app.route("/")
-# def index():
-#     return redirect('/users/')
-
-
 if __name__ == '__main__':
     app = create_app()
     app.run(
@@ -25,13 +9,3 @@ if __name__ == '__main__':
         debug=True,
         port=5000
     )
-
-
-
-
-# from blog.app import app
-# if __name__ == "__main__":
-#     app.run(
-#         host="0.0.0.0",
-#         debug=True,
-#     )
