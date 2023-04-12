7f6872dfaf37f02f19b26364dc38922daf43fb55
roman <belyakov1981@mail.ru>
2023-04-12 15:29:54 +0300

    Lesson 4 create user

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,13 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), unique=True)
     name = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    is_staff = db.Column(db.Boolean, default=False)
 
-    def __init__(self, email, password):
+    def __init__(self, email, password, name, is_staff):
         self.email = email
         self.password = password
+        self.name = name
+        self.is_staff = is_staff
+
+# flask db migrate -m "add is_staff and name field to user model"
+# flask db upgrade
