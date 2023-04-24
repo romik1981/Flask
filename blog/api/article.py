b7139d1a24d1282374d9a1adf9b560188d417251
roman <belyakov1981@mail.ru>
2023-04-24 14:13:46 +0300

    Lesson_10 update to merge

--- blog/api/article.py
+++ blog/api/article.py
@@ -1,57 +1,36 @@
-import click
-from werkzeug.security import generate_password_hash
+from combojsonapi.event.resource import EventsResource
+from flask_combo_jsonapi import ResourceList, ResourceDetail
 
 from blog.extensions import db
+from blog.models import Article
+from blog.schemas import ArticleSchema
 
 
-# @click.command('init-db')
-# def init_db():
-#     from wsgi import app
+class ArticleListEvent(EventsResource):
 
-#     # import models for creating tables
-#     from blog.models import User
-
-#     db.create_all(app=app)
-
-#     print('done!')
+    def event_get_count(self, *args, **kwargs):
+        return {'count': Article.query.count()}
 
 
+class ArticleDetailEvent(EventsResource):
 
-@click.command('create-init-user')
-def create_init_user():
-    
-    from blog.models import User
-    from wsgi import app
-    
-
-    first_name_in = input('first_name: ')
-    last_name_in = input('last_name: ')
-    email_in = input('email: ')
-    password_in = input('password: ')
-    # is_staff_in = input('is_staff: ')
-    
-    # if is_staff_in == '1':
-    #     is_staf_in = True
-    # else: is_staff_in = False
-
-    with app.app_context():
-        db.session.add(
-            User(first_name=first_name_in, last_name=last_name_in, email=email_in, password=generate_password_hash(password_in))
-        )
-        db.session.commit()
-    
-    print('create user: ', first_name_in, last_name_in)
+    def event_get_count_by_author(self, *args, **kwargs):
+        return {'count': Article.query.filter(Article.author_id == kwargs['id']).count()}
 
 
-@click.command('create-init-tags')
-def create_init_tags():
-    from blog.models import Tag
-    from wsgi import app
+class ArticleList(ResourceList):
+    events = ArticleListEvent
+    schema = ArticleSchema
+    data_layer = {
+        'session': db.session,
+        'model': Article,
+    }
 
-    with app.app_context():
-        tags = ('flask', 'django', 'python', 'gb', 'sqlite')
-        for item in tags:
-            db.session.add(Tag(name=item))
-        db.session.commit()
-    click.echo(f'Created tags: {", ".join(tags)}')
- 
+
+class ArticleDetail(ResourceDetail):
+    events = ArticleDetailEvent
+    schema = ArticleSchema
+    data_layer = {
+        'session': db.session,
+        'model': Article,
+    }
